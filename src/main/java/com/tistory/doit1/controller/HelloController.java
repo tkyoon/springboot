d6754aef75a4d9b451b074b99bd19236e5fd2576
TK Yoon <tkyoon@jcone.co.kr>
2021-05-28 15:50:51 +0900

    로그 설정

--- src/main/java/com/tistory/doit1/controller/HelloController.java
+++ src/main/java/com/tistory/doit1/controller/HelloController.java
@@ -1,14 +1,25 @@
 package com.tistory.doit1.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+    private static Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
+        System.out.println("xxx2");
+        log.trace("trace");
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
         return "반갑습니다. doit1.tistory.com에 오신것을 환영합니다.";
     }
-} 
+}
